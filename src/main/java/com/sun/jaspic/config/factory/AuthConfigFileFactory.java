ff1032ff4a535b2922f761bff6439e7c09518918
arjantijms <arjan.tijms@gmail.com>
2019-09-16 14:28:24 +0700

    Formatting of source code

--- src/main/java/com/sun/jaspic/config/factory/AuthConfigFileFactory.java
+++ src/main/java/com/sun/jaspic/config/factory/AuthConfigFileFactory.java
@@ -26,18 +26,13 @@ public class AuthConfigFileFactory extends BaseAuthConfigFactory {
     static volatile RegStoreFileParser regStore = null;
 
     /**
-     * to specialize the defaultEntries passed to the RegStoreFileParser
-     * constructor, create another subclass of BaseAuthconfigFactory, that is
-     * basically a copy of this class, with a change to the third argument
-     * of the call to new ResSToreFileParser. 
-     * to ensure runtime use of the the associated regStore, make sure that
-     * the new subclass also contains an implementation of the getRegStore method.
-     * As done within this class, use the locks defined in
-     * BaseAuthConfigFactory to serialize access to the regStore (both within
-     * the class constructor, and within getRegStore)
+     * to specialize the defaultEntries passed to the RegStoreFileParser constructor, create another subclass of
+     * BaseAuthconfigFactory, that is basically a copy of this class, with a change to the third argument of the call to new
+     * ResSToreFileParser. to ensure runtime use of the the associated regStore, make sure that the new subclass also
+     * contains an implementation of the getRegStore method. As done within this class, use the locks defined in
+     * BaseAuthConfigFactory to serialize access to the regStore (both within the class constructor, and within getRegStore)
      *
-     * All EentyInfo OBJECTS PASSED as deualtEntries MUST HAVE BEEN
-     * CONSTRCTED USING THE FOLLOWING CONSTRUCTOR:
+     * All EentyInfo OBJECTS PASSED as deualtEntries MUST HAVE BEEN CONSTRCTED USING THE FOLLOWING CONSTRUCTOR:
      *
      * EntryInfo(String className, Map<String, String> properties);
      *
@@ -55,8 +50,7 @@ public class AuthConfigFileFactory extends BaseAuthConfigFactory {
         wLock.lock();
         try {
             if (regStore == null) {
-                regStore = new RegStoreFileParser(userDir,
-                        BaseAuthConfigFactory.CONF_FILE_NAME, null);
+                regStore = new RegStoreFileParser(userDir, BaseAuthConfigFactory.CONF_FILE_NAME, null);
                 _loadFactory();
             }
         } finally {
